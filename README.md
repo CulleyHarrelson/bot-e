2a2d818f37fd5afc033295a7aa1c2cf5cb6cc45c
culley <harrelson@gmail.com>
2023-07-29 13:40:04 -0700

    updating site

--- README.md
+++ README.md
@@ -30,3 +30,13 @@ jupyter-lab docs/BottyDocs.ipynb
 The last command will open a jupyterlabs server.  To close the virtual python 
 server use the "deactivate" command.
 
+
+## Installation Issues
+
+Currently the Foundation css files as installed by the npm are not working in layout.pug.
+./node_modules/foundation-icons/foundation-icons.css does not surface the css file.  This 
+is likely an issue with the "public" setting in app.py.  Currently the work around is to 
+
+```bash
+cp -r node_modules/foundation* public/stylesheets/
+```
--- askbotty/app.js
+++ askbotty/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var tosRouter = require('./routes/tos');
 var aboutRouter = require('./routes/about');
+var signupRouter = require('./routes/signup');
 
 var app = express();
 
@@ -19,10 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/tos', tosRouter);
 app.use('/about', aboutRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
